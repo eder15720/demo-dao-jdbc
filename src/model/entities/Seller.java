bda2557e13ca614c46c40d4030f2ae70b98b45eb
Henrique <henri.eder@outlook.com>
2022-11-03 20:52:00 -0300

    adicionando a classe Seller

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable{
+	
+	private static final long serialVersionUID = 5902436549449156268L;
+	
+	private Integer Id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	private Department department;
+	
+	public Seller() {}
+
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
+		Id = id;
+		this.name = name;
+		this.email = email;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(Id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(Id, other.Id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [Id=" + Id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+	
+	
+}
